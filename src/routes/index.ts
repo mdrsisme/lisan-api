68b2538fae928e1f96295d53e8e8328f8db7e839
mdrsisme <mdrsisme@gmail.com>
2026-01-07 21:36:37 +0700

    feat: update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
+
 import authRoutes from './auth.route';
 import userRoutes from './user.route';
 import announcementRoutes from './announcement.route';
+
+import dictionaryRoutes from './dictionary.routes';
+import progressRoutes from './progress.routes';
+import leaderboardRoutes from './leaderboard.routes';
+
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/announcements', announcementRoutes);
+
+router.use('/dictionary', dictionaryRoutes);
+router.use('/progress', progressRoutes);
+router.use('/leaderboard', leaderboardRoutes);
 
 export default router;
